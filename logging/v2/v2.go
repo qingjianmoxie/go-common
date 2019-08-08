86aefa42dd12bc46c938ba0bd0dfb0f6d7503a7d
leel0330 <leel0330@163.com>
2019-08-08 12:24:31 +0000

    D

--- logging/v2/v2.go
+++ logging/v2/v2.go
@@ -1,1 +1,113 @@
 package v2
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+type KV map[string]interface{}
+
+const (
+	InfoStr  = "INFO "
+	ErrorStr = "ERROR "
+)
+
+var logger *JLogger
+
+type JLogger struct {
+	stdLogger     *log.Logger
+	fileLogger    *log.Logger
+	enableFileLog bool
+}
+
+func init() {
+	logger = New()
+}
+
+func New() *JLogger {
+	stdLogger := log.New(os.Stdout, "", log.Lshortfile|log.LstdFlags)
+	return &JLogger{
+		stdLogger:     stdLogger,
+		enableFileLog: false,
+		fileLogger:    nil,
+	}
+}
+
+func (l *JLogger) EnableFileLog(enabled bool) {
+	l.enableFileLog = enabled
+}
+
+func (l *JLogger) SetLogFile(filename string) {
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR|os.O_APPEND, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	if l.fileLogger == nil {
+		fileLogger := log.New(f, "", log.LstdFlags|log.Lshortfile)
+		l.fileLogger = fileLogger
+	} else {
+		l.fileLogger.SetOutput(f)
+	}
+}
+
+func (l *JLogger) Infof(format string, v ...interface{}) {
+	mfmt := fmt.Sprintf("%c[1;40;32m%v%c[0m%v", 0x1B, InfoStr, 0x1B, format)
+	l.stdLogger.Printf(mfmt, v...)
+	if l.enableFileLog {
+		l.fileLogger.Printf(format, v...)
+	}
+}
+
+func (l *JLogger) Errorf(format string, v ...interface{}) {
+	mfmt := fmt.Sprintf("%c[1;40;41m%v%c[0m%v", 0x1B, ErrorStr, 0x1B, format)
+	l.stdLogger.Printf(mfmt, v...)
+	if l.enableFileLog {
+		l.fileLogger.Printf(format, v...)
+	}
+}
+
+func (l *JLogger) InfoKV(kv KV, msg string) {
+	kv["msg"] = msg
+	bytes, _ := json.Marshal(kv)
+	mfmt := fmt.Sprintf("%c[1;40;32m%v%c[0m%v", 0x1B, InfoStr, 0x1B, string(bytes))
+	l.stdLogger.Printf(mfmt)
+	if l.enableFileLog {
+		l.fileLogger.Printf(string(bytes))
+	}
+}
+
+func (l *JLogger) ErrorKV(kv KV, msg string) {
+	kv["msg"] = msg
+	bytes, _ := json.Marshal(kv)
+	mfmt := fmt.Sprintf("%c[1;40;41m%v%c[0m%v", 0x1B, ErrorStr, 0x1B, string(bytes))
+	l.stdLogger.Printf(mfmt)
+	if l.enableFileLog {
+		l.fileLogger.Printf(string(bytes))
+	}
+}
+
+func Infof(format string, v ...interface{}) {
+	logger.Infof(format, v...)
+}
+
+func InfoKV(kv KV, msg string) {
+	logger.InfoKV(kv, msg)
+}
+
+func Errorf(format string, v ...interface{}) {
+	logger.Errorf(format, v...)
+}
+
+func ErrorKV(kv KV, msg string) {
+	logger.ErrorKV(kv, msg)
+}
+
+func EnableFileLog(enabled bool) {
+	logger.EnableFileLog(enabled)
+}
+
+func SetFileLog(filename string) {
+	logger.SetLogFile(filename)
+}
--- logging/v2/v2_test.go
+++ logging/v2/v2_test.go
@@ -1,1 +1,65 @@
 package v2
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"testing"
+)
+
+type MStruct struct {
+	A string `json:"a"`
+	B int    `json:"b"`
+}
+
+func (ms MStruct) String() string {
+	bytes, err := json.Marshal(ms)
+	if err != nil {
+		return ""
+	}
+	return string(bytes)
+}
+
+func TestStdLogger(t *testing.T) {
+	log.Printf("Hello world")
+	log.Printf("Hello %v", "Golang")
+	m := MStruct{A: "Get", B: 123}
+	log.Println("a", 1, m.String())
+}
+
+func TestStd2Logger(t *testing.T) {
+	logger := log.New(os.Stdout, "", log.LstdFlags|log.Lshortfile)
+	logger.Printf("Hello %v", "Logger")
+	logger.Panicf("panic info")
+}
+
+func TestFileLogger(t *testing.T) {
+	file, err := os.OpenFile("logging.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, os.ModePerm)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer file.Close()
+	logger := log.New(file, "", log.LstdFlags|log.Lshortfile)
+
+	logger.Printf("Hello File Log")
+	logger.Printf("Just A OK:a=%v,b=%v", "123", 456)
+}
+
+func TestV2Log(t *testing.T) {
+	Infof("Hello s=%v", "Golang")
+	Errorf("Some Error Emerge:a=%v", 123)
+
+	InfoKV(KV{"age": 1, "name": "jack"}, "Person Info")
+	ErrorKV(KV{"uid": "u12344", "height": 177}, "Error Info")
+}
+
+func TestV2Log1(t *testing.T) {
+	EnableFileLog(true)
+	SetFileLog("../../logging.log")
+
+	Infof("Hello s=%v", "Golang")
+	Errorf("Some Error emerge:a=%v", 123)
+
+	InfoKV(KV{"age": 1, "name": "jack"}, "Person Info")
+	ErrorKV(KV{"uid": "u12344", "height": 177}, "Error Info")
+}
